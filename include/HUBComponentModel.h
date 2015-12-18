dfe436bc4a09cc910311b350a0ec4e9c17fe4c87
John Sundell <josu@spotify.com>
2015-12-18 11:53:56 +0100

    HUBComponentModel: Improve `componentIdentifier` property documentation

--- include/HUBComponentModel.h
+++ include/HUBComponentModel.h
@@ -5,7 +5,14 @@ NS_ASSUME_NONNULL_BEGIN
 /// Protocol defining the public API of a Hub Framework component model. To be implemented.
 @protocol HUBComponentModel <NSObject>
 
-/// The identifier of the component to render the model using
+/**
+ *  The identifier of the component that this model should be rendered using
+ *
+ *  The component identifier should be fully namespaced and match a namespace:component
+ *  combination of a component that has been registered with the Hub Framework.
+ *
+ *  If no component can be resolved for this identifier, a fallback one will be used.
+ */
 @property (nonatomic, copy, readonly) NSString *componentIdentifier;
 
 @end
