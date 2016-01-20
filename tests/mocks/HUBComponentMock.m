5274d4642ae4480636e06f102faef37f2675935d
John Sundell <josu@spotify.com>
2016-01-20 16:39:54 +0100

    HUBComponentMock: Implement HUBComponent API

--- tests/mocks/HUBComponentMock.m
+++ tests/mocks/HUBComponentMock.m
@@ -2,4 +2,31 @@
 
 @implementation HUBComponentMock
 
+@synthesize delegate = _delegate;
+
+- (instancetype)createNewComponent
+{
+    return [HUBComponentMock new];
+}
+
+- (UIView *)loadView
+{
+    return [[UIView alloc] initWithFrame:CGRectZero];
+}
+
+- (CGSize)preferredViewSizeForDisplayingModel:(id<HUBComponentModel>)model containedInViewWithSize:(CGSize)containerViewSize
+{
+    return CGSizeZero;
+}
+
+- (void)prepareViewForReuseWithModel:(id<HUBComponentModel>)model
+{
+    // No-op
+}
+
+- (void)updateViewForLoadedImage:(UIImage *)image forModel:(id<HUBComponentModel>)model
+{
+    // No-op
+}
+
 @end
