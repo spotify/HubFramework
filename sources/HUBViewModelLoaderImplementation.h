9413070f3fa92c6f2411d43592c230f60b37cf0d
John Sundell <josu@spotify.com>
2016-02-08 14:06:48 +0100

    Add HUBViewModelLoader
    Used to load view models based on a combination of remote and local content,
    and building it using an internal `HUBViewModelBuilder`.
    
    The protocol defining the public API for `HUBViewModelLoader` is currently
    not accessible from anywhere, but this will change in the near future.

---
+++ sources/HUBViewModelLoaderImplementation.h
@@ -0,0 +1,32 @@
+#import "HUBViewModelLoader.h"
+
+@protocol HUBRemoteContentProvider;
+@protocol HUBLocalContentProvider;
+@protocol HUBJSONSchema;
+@protocol HUBConnectivityStateResolver;
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// Concrete implementation of the `HUBViewModelLoader` API
+@interface HUBViewModelLoaderImplementation : NSObject <HUBViewModelLoader>
+
+/**
+ *  Initialize an instance of this class with its required dependencies & values
+ *
+ *  @param viewURI The URI of the view that this loader will load view models for
+ *  @param featureIdentifier The identifier of the feature that this loader will belong to
+ *  @param remoteContentProvider Any remote content provider that the loader should use
+ *  @param localContentProvider Any local content provider that the loader should use
+ *  @param JSONSchema The JSON schema that the loader should use for parsing
+ *  @param connectivityStateResolver The connectivity state resolver used by the current `HUBManager`
+ */
+- (instancetype)initWithViewURI:(NSURL *)viewURI
+              featureIdentifier:(NSString *)featureIdentifier
+          remoteContentProvider:(nullable id<HUBRemoteContentProvider>)remoteContentProvider
+           localContentProvider:(nullable id<HUBLocalContentProvider>)localContentProvider
+                     JSONSchema:(id<HUBJSONSchema>)JSONSchema
+      connectivityStateResolver:(id<HUBConnectivityStateResolver>)connectivityStateResolver NS_DESIGNATED_INITIALIZER;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ sources/HUBViewModelLoaderImplementation.m
@@ -0,0 +1,154 @@
+#import "HUBViewModelLoaderImplementation.h"
+
+#import "HUBConnectivityStateResolver.h"
+#import "HUBRemoteContentProvider.h"
+#import "HUBLocalContentProvider.h"
+#import "HUBJSONSchema.h"
+#import "HUBViewModelBuilderImplementation.h"
+#import "HUBViewModelImplementation.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HUBViewModelLoaderImplementation () <HUBRemoteContentProviderDelegate, HUBLocalContentProviderDelegate>
+
+@property (nonatomic, copy, readonly) NSURL *viewURI;
+@property (nonatomic, copy, readonly) NSString *featureIdentifier;
+@property (nonatomic, strong, readonly) id<HUBRemoteContentProvider> remoteContentProvider;
+@property (nonatomic, strong, readonly) id<HUBLocalContentProvider> localContentProvider;
+@property (nonatomic, strong, readonly) id<HUBJSONSchema> JSONSchema;
+@property (nonatomic, strong, readonly) id<HUBConnectivityStateResolver> connectivityStateResolver;
+@property (nonatomic, strong, nullable) HUBViewModelBuilderImplementation *builder;
+
+@end
+
+@implementation HUBViewModelLoaderImplementation
+
+@synthesize delegate = _delegate;
+
+#pragma mark - Public API
+
+- (instancetype)initWithViewURI:(NSURL *)viewURI
+              featureIdentifier:(NSString *)featureIdentifier
+          remoteContentProvider:(nullable id<HUBRemoteContentProvider>)remoteContentProvider
+           localContentProvider:(nullable id<HUBLocalContentProvider>)localContentProvider
+                     JSONSchema:(id<HUBJSONSchema>)JSONSchema
+      connectivityStateResolver:(id<HUBConnectivityStateResolver>)connectivityStateResolver
+{
+    NSParameterAssert(viewURI != nil);
+    NSParameterAssert(featureIdentifier != nil);
+    NSParameterAssert(JSONSchema != nil);
+    NSParameterAssert(connectivityStateResolver != nil);
+    
+    if (!(self = [super init])) {
+        return nil;
+    }
+    
+    _viewURI = viewURI;
+    _featureIdentifier = featureIdentifier;
+    _remoteContentProvider = remoteContentProvider;
+    _localContentProvider = localContentProvider;
+    _JSONSchema = JSONSchema;
+    _connectivityStateResolver = connectivityStateResolver;
+    
+    _remoteContentProvider.delegate = self;
+    _localContentProvider.delegate = self;
+    
+    return self;
+}
+
+#pragma mark - HUBViewModelLoader
+
+- (void)loadViewModel
+{
+    self.builder = [[HUBViewModelBuilderImplementation alloc] initWithFeatureIdentifier:self.featureIdentifier];
+    
+    switch ([self.connectivityStateResolver resolveConnectivityState]) {
+        case HUBConnectivityStateOnline: {
+            if (self.remoteContentProvider != nil) {
+                [self.remoteContentProvider loadContentForViewWithURI:self.viewURI];
+            } else {
+                [self remoteContentPhaseCompletedWithError:nil];
+            }
+            
+            break;
+        }
+        case HUBConnectivityStateOffline:
+            [self remoteContentPhaseCompletedWithError:nil];
+            break;
+    }
+}
+
+#pragma mark - HUBRemoteContentProviderDelegate
+
+- (void)remoteContentProvider:(id<HUBRemoteContentProvider>)contentProvider didLoadJSONData:(NSData *)JSONData
+{
+    NSError *JSONError;
+    NSObject *JSONObject = [NSJSONSerialization JSONObjectWithData:JSONData options:NSJSONReadingAllowFragments error:&JSONError];
+    
+    if (JSONError || JSONObject == nil) {
+        [self remoteContentProvider:contentProvider didFailLoadingWithError:JSONError];
+        return;
+    }
+    
+    if ([JSONObject isKindOfClass:[NSDictionary class]]) {
+        [self.builder addDataFromJSONDictionary:(NSDictionary *)JSONObject usingSchema:self.JSONSchema];
+    } else if ([JSONObject isKindOfClass:[NSArray class]]) {
+        [self.builder addDataFromJSONArray:(NSArray *)JSONObject usingSchema:self.JSONSchema];
+    } else {
+        NSError * const invalidJSONTypeError = [NSError errorWithDomain:@"spotify.com.hubFramework.invalidJSON" code:0 userInfo:nil];
+        [self remoteContentProvider:contentProvider didFailLoadingWithError:invalidJSONTypeError];
+        return;
+    }
+    
+    [self remoteContentPhaseCompletedWithError:nil];
+}
+
+- (void)remoteContentProvider:(id<HUBRemoteContentProvider>)contentProvider didFailLoadingWithError:(NSError *)error
+{
+    [self remoteContentPhaseCompletedWithError:error];
+}
+
+#pragma mark - HUBLocalContentProviderDelegate
+
+- (id<HUBViewModelBuilder>)provideViewModelBuilderForLocalContentProvider:(id<HUBLocalContentProvider>)contentProvider
+{
+    if (self.builder == nil) {
+        self.builder = [[HUBViewModelBuilderImplementation alloc] initWithFeatureIdentifier:self.featureIdentifier];
+    }
+    
+    return self.builder;
+}
+
+- (void)localContentProviderDidLoad:(id<HUBLocalContentProvider>)contentProvider
+{
+    [self allPhasesCompletedWithError:nil];
+}
+
+- (void)localContentProvider:(id<HUBLocalContentProvider>)contentProvider didFailLoadingWithError:(NSError *)error
+{
+    [self allPhasesCompletedWithError:error];
+}
+
+#pragma mark - Private utilities
+
+- (void)remoteContentPhaseCompletedWithError:(nullable NSError *)error
+{
+    if (self.localContentProvider == nil) {
+        [self allPhasesCompletedWithError:error];
+    } else {
+        [self.localContentProvider loadContentForViewWithURI:self.viewURI];
+    }
+}
+
+- (void)allPhasesCompletedWithError:(nullable NSError *)error
+{
+    if (error == nil) {
+        [self.delegate viewModelLoader:self didLoadViewModel:[self.builder build]];
+    } else {
+        [self.delegate viewModelLoader:self didFailLoadingWithError:error];
+    }
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
