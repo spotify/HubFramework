b38cd17c03294aa0e89fa047d97d2bfa27d78ac4
Aron Cedercrantz <cedercra@spotify.com>
2016-02-11 13:44:02 +0100

    Fix build error for partially available method use
    - Adds two macros to make marking code segments which should are OK a
    lot easier. Use them just like `NS_ASSUME_NONNULL_BEGIN/END`.

---
+++ sources/HUBUtilities.h
@@ -0,0 +1,20 @@
+
+/**
+ * Begin a section where the partial availabilty warnings is ignored by the build system.
+ *
+ * Use this macro to silence the compiler after auditing a peice code. Making sure that the code patch is safe even for
+ * deployment targets earlier than when the symbol was introduced.
+ *
+ * Must be paired with with a matching `HUB_IGNORE_PARTIAL_AVAILABILTY_END` statement.
+ */
+#define HUB_IGNORE_PARTIAL_AVAILABILTY_BEGIN \
+    _Pragma("clang diagnostic push") \
+    _Pragma("clang diagnostic ignored \"-Wpartial-availability\"")
+
+/**
+ * End a section where the partial availability warnings is ignored.
+ *
+ * Must be paired with with a matching `HUB_IGNORE_PARTIAL_AVAILABILTY_BEGIN` statement.
+ */
+#define HUB_IGNORE_PARTIAL_AVAILABILTY_END \
+_Pragma("clang diagnostic pop")
