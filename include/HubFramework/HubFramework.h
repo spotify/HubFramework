855bb402a5e8492e2ffdee1d5c402f23bfe53687
John Sundell <josu@spotify.com>
2016-02-12 14:06:47 +0100

    Remove `HUBComponentIdentifier` import from umbrella header

--- include/HubFramework/HubFramework.h
+++ include/HubFramework/HubFramework.h
@@ -32,7 +32,6 @@
 // Components
 #import "HUBComponent.h"
 #import "HUBComponentFactory.h"
-#import "HUBComponentIdentifier.h"
 #import "HUBComponentModel.h"
 #import "HUBComponentModelBuilder.h"
 #import "HUBComponentImageData.h"
