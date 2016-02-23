9113f1ff47e89c3ea809d40fff202d104f6d5edb
John Sundell <josu@spotify.com>
2016-02-23 18:03:45 +0100

    HUBComponent: Refactor image loading callbacks to provide required information

--- include/HubFramework/HUBComponent.h
+++ include/HubFramework/HUBComponent.h
@@ -2,6 +2,7 @@
 
 @protocol HUBComponent;
 @protocol HUBComponentModel;
+@protocol HUBComponentImageData;
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -122,18 +123,29 @@ NS_ASSUME_NONNULL_BEGIN
 
 @optional
 
-#pragma mark - Managing Component Images
+#pragma mark - Handling images
 
 /**
- *  Update this component's view with an image that was loaded for a model
+ *  Return the size that the component prefers that a certain image gets once loaded
+ *
+ *  @param imageData The data that will be used to load the image
+ *  @param model The current model for the component
+ *  @param containerViewSize The size of the container in which the view will be displayed
+ */
+- (CGSize)preferredSizeForImageFromData:(id<HUBComponentImageData>)imageData
+                                  model:(id<HUBComponentModel>)model
+                      containerViewSize:(CGSize)containerViewSize;
+
+/**
+ *  Update the view to display an image that was loaded
  *
  *  @param image The image that was loaded
- *  @param model The model that the image was loded for
- *
- *  You can assume that this method will always be called after `-configureViewWithModel:`,
- *  and that the model will be the same as when that message was sent.
+ *  @param imageData The data that was used to load the image
+ *  @param model The current model for the component
  */
-- (void)updateViewForLoadedImage:(UIImage *)image forModel:(id<HUBComponentModel>)model;
+- (void)updateViewForLoadedImage:(UIImage *)image
+                        fromData:(id<HUBComponentImageData>)imageData
+                           model:(id<HUBComponentModel>)model;
 
 @end
 
