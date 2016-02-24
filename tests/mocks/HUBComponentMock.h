a40ba64699fdb84acd4f16cdbf67dea2ab0cf19b
John Sundell <josu@spotify.com>
2016-02-24 15:39:38 +0100

    HUBComponentMock: Keep track of the image that has been assigned to it

--- tests/mocks/HUBComponentMock.h
+++ tests/mocks/HUBComponentMock.h
@@ -1,9 +1,14 @@
 #import "HUBComponent.h"
 
+@protocol HUBComponentImageData;
+
 NS_ASSUME_NONNULL_BEGIN
 
 /// Mocked component, for use in tests only
 @interface HUBComponentMock : NSObject <HUBComponent>
+
+/// The main image the component is currently displaying
+@property (nonatomic, strong, readonly, nullable) id<HUBComponentImageData> mainImageData;
 
 @end
 
