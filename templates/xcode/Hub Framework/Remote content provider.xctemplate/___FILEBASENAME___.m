ce1648c1b1f2f4fa8556ddf372fb140310767198
John Sundell <josu@spotify.com>
2016-03-10 18:46:42 +0100

    Add Xcode file templates for most commonly created Hub Framework files

---
+++ templates/xcode/Hub Framework/Remote content provider.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,21 @@
+#import "___FILEBASENAME___.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+@synthesize delegate = _delegate;
+
+- (void)loadContentForViewWithURI:(NSURL *)viewURI
+{
+    // Load remote content for a view with a given view URI
+}
+
+- (void)loadContentFromURL:(NSURL *)contentURL
+{
+    // Load remote content from a specific HTTP URL
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
