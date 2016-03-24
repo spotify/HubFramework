4e29e8b9d445d58406d24c0377f864ca50fc242f
John Sundell <josu@spotify.com>
2016-03-24 12:03:44 +0100

    JSON Schema: Mark correct initializer as designated in header file

--- sources/HUBJSONSchemaImplementation.h
+++ sources/HUBJSONSchemaImplementation.h
@@ -10,7 +10,22 @@ NS_ASSUME_NONNULL_BEGIN
  *
  *  @param defaultComponentNamespace The default component namespace of this Hub Framework instance
  */
-- (instancetype)initWithDefaultComponentNamespace:(NSString *)defaultComponentNamespace NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithDefaultComponentNamespace:(NSString *)defaultComponentNamespace;
+
+/**
+ *  Initialize an instance of this class with all required sub-schemas
+ *
+ *  @param viewModelSchema The schema to use for view models
+ *  @param componentModelSchema The schema to use for component models
+ *  @param componentImageDataSchema The schema to use for component image data
+ *  @param defaultComponentNamespace The default component namespace of this Hub Framework instance
+ *
+ *  In order to create default implementations of all sub-schemas, use the convenience initializer.
+ */
+- (instancetype)initWithViewModelSchema:(id<HUBViewModelJSONSchema>)viewModelSchema
+                   componentModelSchema:(id<HUBComponentModelJSONSchema>)componentModelSchema
+               componentImageDataSchema:(id<HUBComponentImageDataJSONSchema>)componentImageDataSchema
+              defaultComponentNamespace:(NSString *)defaultComponentNamespace NS_DESIGNATED_INITIALIZER;
 
 #pragma mark - Unavailable initializers
 
