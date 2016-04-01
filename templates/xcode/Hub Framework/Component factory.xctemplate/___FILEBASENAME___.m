13cbf4fd7dd94a8319518a2b07e46bde73d5c7f1
John Sundell <josu@spotify.com>
2016-04-01 10:20:52 +0200

    Add Xcode file template for `HUBComponentFactory`
    This enables quick setup of a custom component factory from within Xcode

---
+++ templates/xcode/Hub Framework/Component factory.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,15 @@
+#import "___FILEBASENAME___.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (nullable id<HUBComponent>)createComponentForName:(NSString *)name
+{
+    // Return any component that matches `name`, or nil to use a default component.
+    return nil;
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
