745b87c2bf27bd6cf5f77758921ee656d84b3e47
John Sundell <josu@spotify.com>
2016-04-14 13:22:03 +0200

    Add (small) standard library of component categories
    More component categories can still be defined in the application-layer,
    just like with layout traits.
    
    Also modify the layout trait header to be consistent with the category
    one, replacing `extern` with `static`.

---
+++ include/HubFramework/HUBComponentCategories.h
@@ -0,0 +1,23 @@
+#import <Foundation/Foundation.h>
+
+/**
+ *  Type for objects that describe a component category to use for fallbacks using `HUBComponentFallbackHandler`
+ *
+ *  An application using the Hub Framework can declare any number of categories to use when performing fallback logic
+ *  for components, in case an unknown component namespace/name combo was encountered.
+ *
+ *  Ideally, a component category should be generic enough to apply to a range of components with similar visuals and
+ *  behavior, but still contain enough information for a `HUBComponentFallbackHandler` to create appropriate fallback
+ *  components based on them.
+ */
+typedef NSObject HUBComponentCategory;
+
+/// Category for components that have a row-like appearance, with a full screen width and a compact height
+static HUBComponentCategory * const HUBComponentCategoryRow = @"row";
+
+/// Category for components that have a card-like appearance, that are placable in a grid with compact width & height
+static HUBComponentCategory * const HUBComponentCategoryCard = @"card";
+
+/// Category for components that have a carousel-like apperance, with a swipeable horizontal set of child components
+static HUBComponentCategory * const HUBComponentCategoryCarousel = @"carousel";
+
