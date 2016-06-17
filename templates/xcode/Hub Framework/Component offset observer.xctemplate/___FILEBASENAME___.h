d5a34c1e62266e35e70b4417e4b586693965bbda
John Sundell <josu@spotify.com>
2016-06-17 10:44:37 +0200

    Xcode templates: Update for all component protocols + initial content
    This change updates the Xcode templates to include all component protocols,
    and also further explains how initial content operations work.

---
+++ templates/xcode/Hub Framework/Component offset observer.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,9 @@
+#import <HubFramework/HubFramework.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : NSObject <HUBComponentContentOffsetObserver>
+
+@end
+
+NS_ASSUME_NONNULL_END
