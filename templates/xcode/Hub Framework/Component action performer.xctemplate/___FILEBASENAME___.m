e8e7820e91e5447a573b0b3732ab8ede7c50645e
John Sundell <josu@spotify.com>
2016-09-26 11:08:13 +0200

    Add template for HUBComponentActionPerformer
    Also add reference to that protocol in documentation for HUBComponent.

---
+++ templates/xcode/Hub Framework/Component action performer.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,39 @@
+#import "___FILEBASENAME___.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+@synthesize view = _view;
+
+- (NSSet<HUBComponentLayoutTrait *> *)layoutTraits
+{
+    // Return a set of layout traits that describe your component's UI style
+    return [NSSet new];
+}
+
+- (void)loadView
+{
+    // Create your view. You can give it a zero rectangle for its frame.
+    self.view = [[UIView alloc] initWithFrame:CGRectZero];
+}
+
+- (CGSize)preferredViewSizeForDisplayingModel:(id<HUBComponentModel>)model containerViewSize:(CGSize)containerViewSize
+{
+    // Return the size you'd prefer that the layout system resizes your view to
+    return CGSizeZero;
+}
+
+- (void)prepareViewForReuse
+{
+    // Prepare your view for reuse, reset state, remove highlights, etc.
+}
+
+- (void)configureViewWithModel:(id<HUBComponentModel>)model containerViewSize:(CGSize)containerViewSize
+{
+    // Do your model->view data binding here
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
