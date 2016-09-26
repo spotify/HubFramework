351daee3fec777223cd90ac25c27aac7a88ca741
John Sundell <josu@spotify.com>
2016-09-26 11:26:06 +0200

    Update content operation file templates

---
+++ templates/xcode/Hub Framework/Cont operation + action observer.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,26 @@
+#import "___FILEBASENAME___.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (void)performForViewURI:(NSURL *)viewURI
+              featureInfo:(id<HUBFeatureInfo>)featureInfo
+        connectivityState:(HUBConnectivityState)connectivityState
+         viewModelBuilder:(id<HUBViewModelBuilder>)viewModelBuilder
+            previousError:(nullable NSError *)previousError
+{
+    // Perform the content operation, and call the delegate once done
+}
+
+- (void)actionPerformedWithContext:(id<HUBActionContext>)context
+                           viewURI:(NSURL *)viewURI
+                       featureInfo:(id<HUBFeatureInfo>)featureInfo
+                 connectivityState:(HUBConnectivityState)connectivityState
+{
+    // React to that an action was performed in the content operation's view
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
