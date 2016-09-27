34fae16fd607e1cc0ee62d5dc3772482c3cf7bc1
John Sundell <josu@spotify.com>
2016-09-27 16:24:02 +0200

    [Demo app] Add view URI for GitHub Search + use scheme
    Also register the URI scheme with the system.

--- demo/sources/URL+ViewURIs.swift
+++ demo/sources/URL+ViewURIs.swift
@@ -3,6 +3,17 @@ import Foundation
 extension URL {
     /// The view URI used for the "Root" feature
     static var rootViewURI: URL {
-        return URL(string: "root")!
+        return URL(viewURI: "root")
+    }
+    
+    // The view URI used for the "GitHub search" feature
+    static var gitHubSearchViewURI: URL {
+        return URL(viewURI: "githubsearch")
+    }
+}
+
+private extension URL {
+    init(viewURI: String) {
+        self.init(string: "hub-demo:" + viewURI)!
     }
 }
