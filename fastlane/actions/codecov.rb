c07e3356bdb1716f2f1cbc485a96dcf444bc3ef8
Aron Cedercrantz <cedercra@spotify.com>
2016-09-27 17:32:41 -0400

    Change to pass Codecov URL as a parameter

--- fastlane/actions/codecov.rb
+++ fastlane/actions/codecov.rb
@@ -8,7 +8,7 @@ module Fastlane
         dd_path = params[:derived_data_path] || Actions.lane_context[:SCAN_DERIVED_DATA_PATH]
         UI.user_error!("Invalid derived_data_path") unless dd_path
 
-        cc_url = "https://codecov.spotify.net"
+        cc_url = params[:url]
         cc_token = params[:token]
         pr = (Actions::GitBranchAction.run({}) || '').scan(/pull\/(\d+)\//).flatten.first
 
@@ -41,6 +41,11 @@ module Fastlane
                                        env_name: "CODECOV_TOKEN",
                                        description: "The Codecov upload token"),
 
+          FastlaneCore::ConfigItem.new(key: :url,
+                                       env_name: "CODECOV_URL",
+                                       description: "The URL of the Codecov instance",
+                                       default_value: "https://codecov.io"),
+ 
           FastlaneCore::ConfigItem.new(key: :derived_data_path,
                                        env_name: "CODECOV_DERIVED_DATA_PATH",
                                        description: "Path to the derived data directory",
