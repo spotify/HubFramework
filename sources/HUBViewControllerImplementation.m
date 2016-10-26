a3ff0af027f116cb86521ea2ca92af4e57d36b69
Krzysztof Werys <kwerys@spotify.com>
2016-10-26 19:12:04 +0200

    Rename a variable.

--- sources/HUBViewControllerImplementation.m
+++ sources/HUBViewControllerImplementation.m
@@ -1199,14 +1199,14 @@ willUpdateSelectionState:(HUBComponentSelectionState)selectionState
                                                                                   componentModel:componentModel
                                                                                   viewController:self];
 
-    BOOL isActionHandled = [self.actionHandler handleActionWithContext:context];
+    BOOL actionWasHandled = [self.actionHandler handleActionWithContext:context];
 
     for (HUBComponentWrapper *componentWrapper in self.actionObservingComponentWrappers) {
         id<HUBComponentActionObserver> observer = componentWrapper;
         [observer actionPerformedWithContext:context];
     }
 
-    return isActionHandled;
+    return actionWasHandled;
 }
 
 - (void)addComponentWrapperToLookupTables:(nullable HUBComponentWrapper *)componentWrapper
