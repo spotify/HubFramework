4c6738edb923bd01d9e2928839e2496daf4b093e
John Sundell <josu@spotify.com>
2016-10-29 23:44:25 +0200

    Demo app: Use DEBUG instead of HUB_DEBUG

--- demo/sources/AppDelegate.swift
+++ demo/sources/AppDelegate.swift
@@ -161,7 +161,7 @@ import HubFramework
     }
     
     private func startLiveService() {
-        #if HUB_DEBUG
+        #if DEBUG
         hubManager.liveService?.delegate = self
         hubManager.liveService?.start(onPort: 7777)
         #endif
