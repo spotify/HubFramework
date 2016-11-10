20f42b11ca99bcae5b6283e6694feae7a428e7d2
Nataliya Patsovska <nataliya@spotify.com>
2016-11-10 15:59:44 +0200

    Add accessibility identifier to the collection view
    The accessibility identifier could be used for automated UI tests.

--- sources/HUBCollectionViewFactory.m
+++ sources/HUBCollectionViewFactory.m
@@ -27,10 +27,12 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (UICollectionView *)createCollectionView
 {
-    return [[UICollectionView alloc] initWithFrame:CGRectZero
-                              collectionViewLayout:[UICollectionViewLayout new]];
-}
+    UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero
+                                                          collectionViewLayout:[UICollectionViewLayout new]];
+    collectionView.accessibilityIdentifier = @"collectionView";
 
+    return collectionView;
+}
 @end
 
 NS_ASSUME_NONNULL_END
