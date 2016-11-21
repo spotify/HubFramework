9de822697e3419b58299424a3f5432bf6d81996a
John Sundell <josu@spotify.com>
2016-11-21 13:23:16 +0100

    Move `viewControllerShouldIgnoreHeaderComponentContentInset` into calculation method

--- sources/HUBViewController.m
+++ sources/HUBViewController.m
@@ -963,11 +963,13 @@ willUpdateSelectionState:(HUBComponentSelectionState)selectionState
 - (CGFloat)calculateTopContentInset
 {
     if (self.headerComponentWrapper != nil) {
-        HUBComponentWrapper * const headerComponentWrapper = self.headerComponentWrapper;
-        CGSize const defaultHeaderSize = [headerComponentWrapper preferredViewSizeForDisplayingModel:headerComponentWrapper.model
-                                                                                   containerViewSize:self.collectionView.frame.size];
-        
-        return defaultHeaderSize.height;
+        if (![self.delegate viewControllerShouldIgnoreHeaderComponentContentInset:self]) {
+            HUBComponentWrapper * const headerComponentWrapper = self.headerComponentWrapper;
+            CGSize const defaultHeaderSize = [headerComponentWrapper preferredViewSizeForDisplayingModel:headerComponentWrapper.model
+                                                                                       containerViewSize:self.collectionView.frame.size];
+            
+            return defaultHeaderSize.height;
+        }
     }
     
     CGFloat const statusBarWidth = CGRectGetWidth([UIApplication sharedApplication].statusBarFrame);
