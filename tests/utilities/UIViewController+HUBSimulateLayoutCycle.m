ef6aefbf848c07a9a83e75f75343f9039a309f68
John Sundell <josu@spotify.com>
2016-11-30 13:35:46 +0100

    UIViewController+SimulateLayout: Access self.view instead of calling -loadView

--- tests/utilities/UIViewController+HUBSimulateLayoutCycle.m
+++ tests/utilities/UIViewController+HUBSimulateLayoutCycle.m
@@ -4,8 +4,9 @@
 
 - (void)hub_simulateLayoutCycle
 {
-    [self loadView];
-    [self viewDidLoad];
+    // Load the view
+    __unused UIView * const _ = self.view;
+    
     [self viewWillAppear:YES];
     self.view.frame = CGRectMake(0, 0, 320, 400);
     [self viewDidLayoutSubviews];
