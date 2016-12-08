93cd9bf3c9ca170594e7ed1e4888d75c55e6dc29
Ceri Hughes <github@cerihughes.co.uk>
2016-12-08 13:48:10 +0100

    Move model creation utilities to a separate class

--- tests/HUBViewModelDiffTests.m
+++ tests/HUBViewModelDiffTests.m
@@ -24,6 +24,7 @@
 #import "HUBIdentifier.h"
 #import "HUBComponentModelImplementation.h"
 #import "HUBViewModelImplementation.h"
+#import "HUBViewModelUtilities.h"
 
 #import <XCTest/XCTest.h>
 
@@ -33,55 +34,18 @@
 
 @implementation HUBViewModelDiffTests
 
-- (id<HUBComponentModel>)createComponentModelWithIdentifier:(NSString *)identifier
-                                                 customData:(nullable NSDictionary *)customData
-{
-    HUBIdentifier * const componentIdentifier = [[HUBIdentifier alloc] initWithNamespace:@"namespace" name:@"name"];
-    return [[HUBComponentModelImplementation alloc] initWithIdentifier:identifier
-                                                                  type:HUBComponentTypeBody
-                                                                 index:0
-                                                       groupIdentifier:nil
-                                                   componentIdentifier:componentIdentifier
-                                                     componentCategory:HUBComponentCategoryBanner
-                                                                 title:@"title"
-                                                              subtitle:@"subtitle"
-                                                        accessoryTitle:nil
-                                                       descriptionText:nil
-                                                         mainImageData:nil
-                                                   backgroundImageData:nil
-                                                       customImageData:@{}
-                                                                  icon:nil
-                                                                target:nil
-                                                              metadata:nil
-                                                           loggingData:nil
-                                                            customData:customData
-                                                                parent:nil];
-}
-
-- (id<HUBViewModel>)createViewModelWithIdentifier:(NSString *)identifier components:(NSArray<id<HUBComponentModel>> *)components
-{
-    UINavigationItem * const navigationItem = [[UINavigationItem alloc] initWithTitle:@"Title"];
-    
-    return [[HUBViewModelImplementation alloc] initWithIdentifier:identifier
-                                                   navigationItem:navigationItem
-                                             headerComponentModel:nil
-                                              bodyComponentModels:components
-                                           overlayComponentModels:@[]
-                                                       customData:@{@"custom": @"data"}];
-}
-
 - (void)testInsertions
 {
-    id<HUBViewModel> firstViewModel = [self createViewModelWithIdentifier:@"Test"
-                                                               components:@[]];
+    id<HUBViewModel> firstViewModel = [HUBViewModelUtilities createViewModelWithIdentifier:@"Test"
+                                                                                components:@[]];
     NSArray<id<HUBComponentModel>> *secondComponents = @[
-        [self createComponentModelWithIdentifier:@"component-1" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-2" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-3" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-4" customData:nil]
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-1" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-2" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-3" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-4" customData:nil]
     ];
-    id<HUBViewModel> secondViewModel = [self createViewModelWithIdentifier:@"Test"
-                                                                components:secondComponents];
+    id<HUBViewModel> secondViewModel = [HUBViewModelUtilities createViewModelWithIdentifier:@"Test"
+                                                                                 components:secondComponents];
 
     HUBViewModelDiff *diff = [HUBViewModelDiff diffFromViewModel:firstViewModel toViewModel:secondViewModel];
     XCTAssert([diff.insertedBodyComponentIndexPaths containsObject:[NSIndexPath indexPathForItem:0 inSection:0]]);
@@ -96,21 +60,21 @@
 - (void)testReloads
 {
     NSArray<id<HUBComponentModel>> *firstComponents = @[
-        [self createComponentModelWithIdentifier:@"component-1" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-2" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-3" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-4" customData:nil]
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-1" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-2" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-3" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-4" customData:nil]
     ];
-    id<HUBViewModel> firstViewModel = [self createViewModelWithIdentifier:@"Test"
-                                                            components:firstComponents];
+    id<HUBViewModel> firstViewModel = [HUBViewModelUtilities createViewModelWithIdentifier:@"Test"
+                                                                                components:firstComponents];
     NSArray<id<HUBComponentModel>> *secondComponents = @[
-        [self createComponentModelWithIdentifier:@"component-1" customData:@{@"test": @5}],
-        [self createComponentModelWithIdentifier:@"component-2" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-3" customData:@{@"test": @6}],
-        [self createComponentModelWithIdentifier:@"component-4" customData:nil]
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-1" customData:@{@"test": @5}],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-2" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-3" customData:@{@"test": @6}],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-4" customData:nil]
     ];
-    id<HUBViewModel> secondViewModel = [self createViewModelWithIdentifier:@"Test"
-                                                                components:secondComponents];
+    id<HUBViewModel> secondViewModel = [HUBViewModelUtilities createViewModelWithIdentifier:@"Test"
+                                                                                 components:secondComponents];
 
     HUBViewModelDiff *diff = [HUBViewModelDiff diffFromViewModel:firstViewModel toViewModel:secondViewModel];
     XCTAssert([diff.reloadedBodyComponentIndexPaths containsObject:[NSIndexPath indexPathForItem:0 inSection:0]]);
@@ -124,19 +88,19 @@
 - (void)testDeletions
 {
     NSArray<id<HUBComponentModel>> *firstComponents = @[
-        [self createComponentModelWithIdentifier:@"component-1" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-2" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-3" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-4" customData:nil]
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-1" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-2" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-3" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-4" customData:nil]
     ];
-    id<HUBViewModel> firstViewModel = [self createViewModelWithIdentifier:@"Test"
-                                                            components:firstComponents];
+    id<HUBViewModel> firstViewModel = [HUBViewModelUtilities createViewModelWithIdentifier:@"Test"
+                                                                                components:firstComponents];
     NSArray<id<HUBComponentModel>> *secondComponents = @[
-        [self createComponentModelWithIdentifier:@"component-2" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-4" customData:nil]
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-2" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-4" customData:nil]
     ];
-    id<HUBViewModel> secondViewModel = [self createViewModelWithIdentifier:@"Test"
-                                                                components:secondComponents];
+    id<HUBViewModel> secondViewModel = [HUBViewModelUtilities createViewModelWithIdentifier:@"Test"
+                                                                                 components:secondComponents];
 
     HUBViewModelDiff *diff = [HUBViewModelDiff diffFromViewModel:firstViewModel toViewModel:secondViewModel];
     XCTAssert([diff.deletedBodyComponentIndexPaths containsObject:[NSIndexPath indexPathForItem:0 inSection:0]]);
@@ -149,33 +113,33 @@
 - (void)testComplexChangeSet
 {
     NSArray<id<HUBComponentModel>> *firstComponents = @[
-        [self createComponentModelWithIdentifier:@"component-1" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-2" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-3" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-4" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-5" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-6" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-7" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-8" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-9" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-10" customData:nil]
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-1" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-2" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-3" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-4" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-5" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-6" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-7" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-8" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-9" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-10" customData:nil]
     ];
-    id<HUBViewModel> firstViewModel = [self createViewModelWithIdentifier:@"Test"
-                                                               components:firstComponents];
+    id<HUBViewModel> firstViewModel = [HUBViewModelUtilities createViewModelWithIdentifier:@"Test"
+                                                                                components:firstComponents];
     NSArray<id<HUBComponentModel>> *secondComponents = @[
-        [self createComponentModelWithIdentifier:@"component-1" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-2" customData:@{@"test": @1}],
-        [self createComponentModelWithIdentifier:@"component-30" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-4" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-5" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-6" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-7" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-9" customData:@{@"test": @2}],
-        [self createComponentModelWithIdentifier:@"component-10" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-13" customData:nil]
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-1" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-2" customData:@{@"test": @1}],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-30" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-4" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-5" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-6" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-7" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-9" customData:@{@"test": @2}],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-10" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-13" customData:nil]
     ];
-    id<HUBViewModel> secondViewModel = [self createViewModelWithIdentifier:@"Test"
-                                                                components:secondComponents];
+    id<HUBViewModel> secondViewModel = [HUBViewModelUtilities createViewModelWithIdentifier:@"Test"
+                                                                                 components:secondComponents];
 
     HUBViewModelDiff *diff = [HUBViewModelDiff diffFromViewModel:firstViewModel toViewModel:secondViewModel];
     XCTAssert([diff.deletedBodyComponentIndexPaths containsObject:[NSIndexPath indexPathForItem:2 inSection:0]]);
@@ -192,20 +156,20 @@
 - (void)testInsertionOfSingleComponentModelAtStartWithDataChanges
 {
     NSArray<id<HUBComponentModel>> *firstComponents = @[
-        [self createComponentModelWithIdentifier:@"component-1" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-2" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-3" customData:nil]
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-1" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-2" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-3" customData:nil]
     ];
-    id<HUBViewModel> firstViewModel = [self createViewModelWithIdentifier:@"Test"
-                                                               components:firstComponents];
+    id<HUBViewModel> firstViewModel = [HUBViewModelUtilities createViewModelWithIdentifier:@"Test"
+                                                                                components:firstComponents];
     NSArray<id<HUBComponentModel>> *secondComponents = @[
-        [self createComponentModelWithIdentifier:@"component-0" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-1" customData:@{@"test": @1}],
-        [self createComponentModelWithIdentifier:@"component-2" customData:@{@"test": @1}],
-        [self createComponentModelWithIdentifier:@"component-3" customData:@{@"test": @1}]
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-0" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-1" customData:@{@"test": @1}],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-2" customData:@{@"test": @1}],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-3" customData:@{@"test": @1}]
     ];
-    id<HUBViewModel> secondViewModel = [self createViewModelWithIdentifier:@"Test"
-                                                                components:secondComponents];
+    id<HUBViewModel> secondViewModel = [HUBViewModelUtilities createViewModelWithIdentifier:@"Test"
+                                                                                 components:secondComponents];
 
     HUBViewModelDiff *diff = [HUBViewModelDiff diffFromViewModel:firstViewModel toViewModel:secondViewModel];
     XCTAssert(diff.reloadedBodyComponentIndexPaths.count == 3);
@@ -220,21 +184,21 @@
 - (void)testInsertionOfMultipleComponentModelsAtStartWithDataChanges
 {
     NSArray<id<HUBComponentModel>> *firstComponents = @[
-        [self createComponentModelWithIdentifier:@"component-1" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-2" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-3" customData:nil]
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-1" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-2" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-3" customData:nil]
     ];
-    id<HUBViewModel> firstViewModel = [self createViewModelWithIdentifier:@"Test"
-                                                               components:firstComponents];
+    id<HUBViewModel> firstViewModel = [HUBViewModelUtilities createViewModelWithIdentifier:@"Test"
+                                                                                components:firstComponents];
     NSArray<id<HUBComponentModel>> *secondComponents = @[
-        [self createComponentModelWithIdentifier:@"component-0" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-00" customData:nil],
-        [self createComponentModelWithIdentifier:@"component-1" customData:@{@"test": @1}],
-        [self createComponentModelWithIdentifier:@"component-2" customData:@{@"test": @1}],
-        [self createComponentModelWithIdentifier:@"component-3" customData:@{@"test": @1}]
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-0" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-00" customData:nil],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-1" customData:@{@"test": @1}],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-2" customData:@{@"test": @1}],
+        [HUBViewModelUtilities createComponentModelWithIdentifier:@"component-3" customData:@{@"test": @1}]
     ];
-    id<HUBViewModel> secondViewModel = [self createViewModelWithIdentifier:@"Test"
-                                                                components:secondComponents];
+    id<HUBViewModel> secondViewModel = [HUBViewModelUtilities createViewModelWithIdentifier:@"Test"
+                                                                                 components:secondComponents];
 
     HUBViewModelDiff *diff = [HUBViewModelDiff diffFromViewModel:firstViewModel toViewModel:secondViewModel];
     XCTAssert(diff.reloadedBodyComponentIndexPaths.count == 3);
---
+++ tests/utilities/HUBViewModelUtilities.m
@@ -0,0 +1,70 @@
+/*
+ *  Copyright (c) 2016 Spotify AB.
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+#import "HUBViewModelUtilities.h"
+
+#import "HUBComponentModelImplementation.h"
+#import "HUBViewModelImplementation.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@implementation HUBViewModelUtilities
+
++ (id<HUBComponentModel>)createComponentModelWithIdentifier:(NSString *)identifier
+                                                 customData:(nullable NSDictionary *)customData
+{
+    HUBIdentifier * const componentIdentifier = [[HUBIdentifier alloc] initWithNamespace:@"namespace" name:@"name"];
+    return [[HUBComponentModelImplementation alloc] initWithIdentifier:identifier
+                                                                  type:HUBComponentTypeBody
+                                                                 index:0
+                                                       groupIdentifier:nil
+                                                   componentIdentifier:componentIdentifier
+                                                     componentCategory:HUBComponentCategoryBanner
+                                                                 title:@"title"
+                                                              subtitle:@"subtitle"
+                                                        accessoryTitle:nil
+                                                       descriptionText:nil
+                                                         mainImageData:nil
+                                                   backgroundImageData:nil
+                                                       customImageData:@{}
+                                                                  icon:nil
+                                                                target:nil
+                                                              metadata:nil
+                                                           loggingData:nil
+                                                            customData:customData
+                                                                parent:nil];
+}
+
++ (id<HUBViewModel>)createViewModelWithIdentifier:(NSString *)identifier components:(NSArray<id<HUBComponentModel>> *)components
+{
+    UINavigationItem * const navigationItem = [[UINavigationItem alloc] initWithTitle:@"Title"];
+
+    return [[HUBViewModelImplementation alloc] initWithIdentifier:identifier
+                                                   navigationItem:navigationItem
+                                             headerComponentModel:nil
+                                              bodyComponentModels:components
+                                           overlayComponentModels:@[]
+                                                       customData:@{@"custom": @"data"}];
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
