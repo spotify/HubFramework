6e24d42dfea81b4321e97d0ed2650f2df3fdc0ee
Krzysztof Werys <kwerys@spotify.com>
2016-12-16 13:59:47 +0100

    Remove unused code introduced by mistake in previous PR.

--- sources/HUBViewControllerDefaultScrollHandler.m
+++ sources/HUBViewControllerDefaultScrollHandler.m
@@ -52,12 +52,6 @@ NS_ASSUME_NONNULL_BEGIN
     return proposedContentInsets;
 }
 
-- (CGPoint)centerPointForOverlayComponentInViewController:(HUBViewController *)viewController
-                                      proposedCenterPoint:(CGPoint)proposedCenterPoint
-{
-    return proposedCenterPoint;
-}
-
 - (void)scrollingWillStartInViewController:(HUBViewController *)viewController
                         currentContentRect:(CGRect)currentContentRect
 {
