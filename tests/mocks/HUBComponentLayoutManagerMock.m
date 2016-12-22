1fa4c0e5a966e2fc481609c420eb9444e90ca665
Ceri Hughes <github@cerihughes.co.uk>
2016-12-22 13:55:50 +0000

    Allow tests to compile on 32 bit

--- tests/mocks/HUBComponentLayoutManagerMock.m
+++ tests/mocks/HUBComponentLayoutManagerMock.m
@@ -41,32 +41,32 @@
 - (CGFloat)marginBetweenComponentWithLayoutTraits:(NSSet<HUBComponentLayoutTrait> *)layoutTraits
                                    andContentEdge:(HUBComponentLayoutContentEdge)contentEdge
 {
-    return [self.contentEdgeMarginsForLayoutTraits[layoutTraits] doubleValue];
+    return (CGFloat)[self.contentEdgeMarginsForLayoutTraits[layoutTraits] doubleValue];
 }
 
 - (CGFloat)verticalMarginBetweenComponentWithLayoutTraits:(NSSet<HUBComponentLayoutTrait> *)layoutTraits
                        andHeaderComponentWithLayoutTraits:(NSSet<HUBComponentLayoutTrait> *)headerLayoutTraits
 {
-    return [self.headerMarginsForLayoutTraits[layoutTraits] doubleValue];
+    return (CGFloat)[self.headerMarginsForLayoutTraits[layoutTraits] doubleValue];
 }
 
 - (CGFloat)horizontalMarginForComponentWithLayoutTraits:(NSSet<HUBComponentLayoutTrait> *)layoutTraits
                          precedingComponentLayoutTraits:(NSSet<HUBComponentLayoutTrait> *)precedingComponentLayoutTraits
 {
-    return [self.horizontalComponentMarginsForLayoutTraits[layoutTraits] doubleValue];
+    return (CGFloat)[self.horizontalComponentMarginsForLayoutTraits[layoutTraits] doubleValue];
 }
 
 - (CGFloat)verticalMarginForComponentWithLayoutTraits:(NSSet<HUBComponentLayoutTrait> *)layoutTraits
                        precedingComponentLayoutTraits:(NSSet<HUBComponentLayoutTrait> *)precedingComponentLayoutTraits
 {
-    return [self.verticalComponentMarginsForLayoutTraits[layoutTraits] doubleValue];
+    return (CGFloat)[self.verticalComponentMarginsForLayoutTraits[layoutTraits] doubleValue];
 }
 
 - (CGFloat)horizontalOffsetForComponentsWithLayoutTraits:(NSArray<NSSet<HUBComponentLayoutTrait> *> *)componentsTraits
                    firstComponentLeadingHorizontalOffset:(CGFloat)firstComponentLeadingOffsetX
                    lastComponentTrailingHorizontalOffset:(CGFloat)lastComponentTrailingOffsetX
 {
-    return [self.horizontalComponentOffsetsForArrayOfLayoutTraits[componentsTraits] doubleValue];
+    return (CGFloat)[self.horizontalComponentOffsetsForArrayOfLayoutTraits[componentsTraits] doubleValue];
 }
 
 @end
