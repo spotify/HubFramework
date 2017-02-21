92f346e82ff9a444b070f398aa3ff444120dfc31
bubski <hi@bubski.pl>
2017-02-21 11:08:02 +0100

    Protocol and concrete class for synchronizing gestures

---
+++ sources/HUBGestureRecognizerSynchronizing.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol HUBGestureRecognizerSynchronizing
+@property (nonatomic, assign, getter=isLocked) BOOL locked;
+@end
+
+NS_ASSUME_NONNULL_END
